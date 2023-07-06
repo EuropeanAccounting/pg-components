ddb425f4c811aac14ba5e88821d2a7708f84d124
Lisandro Rocha <lisandro@itcplatform.de>
2023-07-06 12:05:33 +0200

    new notifications added

--- src/components/notifications/index.ts
+++ src/components/notifications/index.ts
@@ -1,3 +1,4 @@
 export { default as Alert } from "./Alert";
 export { default as Badge } from "./Badge";
 export { default as FormError } from "./FormError";
+export { default as Notification } from "./Notification";
